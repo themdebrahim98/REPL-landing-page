2660f806c9c1194877e67e25cb1b2487528394db
md ebrahim <mdebrahim742148@gmail.com>
2021-04-24 15:09:47 +0530

    my first repl website

---
+++ index.js
@@ -0,0 +1,11 @@
+const hamburger = document.querySelector('.hamburger');
+const mobile_menu = document.querySelector('.list');
+const header  = document.querySelector('.header.container');
+
+hamburger.addEventListener('click',()=>{
+    console.log("lasbchacghwv")
+
+
+    hamburger.classList.toggle("active");
+    mobile_menu.classList.toggle("active2");
+})
